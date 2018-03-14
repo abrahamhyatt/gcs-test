9917251ea6a2cc50fd27c3fe39f9a9c6f5677526
Abraham Hyatt <abrahamhyatt@gmail.com>
2018-03-14 09:23:11 -0700

    Revert "replaced coreutils sort with randomize-lines"
    This reverts commit 0d4722361601b79b16d9b4f5898e7a411ee36df5.

--- gcs-test.sh
+++ gcs-test.sh
@@ -2,9 +2,9 @@
 
 tmp=dic/tmp
 
-rl dic/gcs_eye | head -n 1 > $tmp
-rl dic/gcs_verbal | head -n 1 >> $tmp
-rl dic/gcs_motor | head -n 1 >> $tmp
+sort -R dic/gcs_eye | head -n 1 > $tmp
+sort -R dic/gcs_verbal | head -n 1 >> $tmp
+sort -R dic/gcs_motor | head -n 1 >> $tmp
 awk -F '\t' '{print $1}' $tmp
 read -p "Score: " ans
 score=$(awk -F '\t' '{ sum+=$2} END {print sum}' $tmp)
